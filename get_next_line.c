10a551858d418af29be24c9ce28d9b990317f9cc
Mehdi Buchet <mbuchet@student.42belgium.be>
2026-05-04 22:51:09 +0200

    test: make get_next_line_end remove malloc for content from buf

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,50 @@
 /*   By: mbuchet <mbuchet@student.42belgium.be>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/01 20:05:35 by mbuchet           #+#    #+#             */
-/*   Updated: 2026/05/04 20:15:55 by mbuchet          ###   ########.fr       */
+/*   Updated: 2026/05/04 22:50:20 by mbuchet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
+
+/*static void	make_buffer_for_get_next_line(t_buffer *buf, char *line, size_t new_size)
+{
+	size_t	cut_head;
+	char 	array[new_size];
+
+	if (buf->size == 0)
+		return ;
+	cut_head = buf->size - buf->current_read;
+	if (new_size > buf->size)
+		cut_head = 0;
+	else
+		cut_head = buf->size - new_size;
+	ft_strlcpy(array, buf->content + cut_head, buf->size);
+	array[new_size] = 0;
+	//free(buf->content);
+	//buf->content = array;
+	//buf->current_read -= cut_head;
+	printf("[ar size: %zu, arr: %s]", new_size, array);
+}
+
+static void	make_buffer_for_get_next_line2(t_buffer *buf, size_t new_size)
+{
+	size_t	cut_head;
+	char	array[new_size];
+
+	if (new_size > buf->size)
+		cut_head = 0;
+	else
+		cut_head = buf->size - new_size;
+	buf->size = buf->size - cut_head;
+	if (buf->content != NULL)
+		ft_strlcpy(array, buf->content + cut_head, buf->size);
+	free(buf->content);
+	array[new_size] = 0;
+	buf->content = array;
+	buf->current_read -= cut_head;
+}*/
 
 static char	*extract_line(t_buffer *buf)
 {
@@ -31,7 +70,10 @@ static char	*extract_line(t_buffer *buf)
 		return (NULL);
 	line[buf->current_read] = 0;
 	ft_strlcpy(line, buf->content, buf->current_read);
-	buffer_realloc_head(buf, buf->size - buf->current_read);
+	//printf("[before : %s, %s, %zu, %zu]\n", buf->content, line, buf->current_read, buf->size);
+	//buffer_realloc_head(buf, buf->size - buf->current_read);
+	make_buffer_for_get_next_line2(buf, buf->size - buf->current_read);
+	//printf("[after : %s, %s, %zu, %zu]\n", buf->content, line, buf->current_read, buf->size);
 	return (line);
 }
 
@@ -44,6 +86,21 @@ static ssize_t	current_read_line(t_buffer *buf, int fd)
 	if (bytes_read != -1)
 		buf->size += bytes_read;
 	return (bytes_read);
+}
+
+static	char*	get_next_line_end(t_buffer *buf, char *line, int fd)
+{
+	if (buf->size == 0)
+	{
+		free_buf(buf);
+		return (NULL);
+	}
+	buffer_realloc_head(buf, buf->size + 1);
+	line = buf->content;
+	line[buf->size] = 0;
+	buf->content = NULL;
+	free_buf(buf);
+	return (line);
 }
 
 char	*get_next_line(int fd)
@@ -62,13 +119,7 @@ char	*get_next_line(int fd)
 		result_read = current_read_line(&buf, fd);
 		if (result_read == -1 || result_read == 0)
 		{
-			if (buf.size == 0)
-				return (NULL);
-			buffer_realloc_head(&buf, buf.size + 1);
-			line = buf.content;
-			line[buf.size] = 0;
-			buf.content = NULL;
-			free_buf(&buf);
+			line = get_next_line_end(&buf, line, fd);
 			break ;
 		}
 	}
